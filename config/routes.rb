81dcfcaf9ef5854b622d4bfc60a0fe76020f0001
mana <m.komori6517@gmail.com>
2024-10-23 21:18:04 +0900

    fix: rubocop

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   root "static_pages#top"
   devise_for :users, controllers: {
-    registrations: 'users/registrations'
+    registrations: "users/registrations"
   }
   resources :recommends
-  
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
