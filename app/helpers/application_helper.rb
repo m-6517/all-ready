9eea8d372cbb8b32c35d90ad2561b8e69276a297
mana <m.komori6517@gmail.com>
2024-10-23 23:44:06 +0900

    add: フラッシュメッセージ

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  def flash_background_color(type)
+    case type.to_sym
+    when :notice then "bg-sky-100 text-sky-700"
+    when :alert then "bg-sky-100 text-sky-700"
+    end
+  end
 end
