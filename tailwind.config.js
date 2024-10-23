82b3b1d7475419bd50d72be7cea1f71c6906358c
mana <m.komori6517@gmail.com>
2024-10-23 13:45:23 +0900

    add: daisyUIのカラーテーマ

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,4 +6,20 @@ module.exports = {
     './app/javascript/**/*.js'
   ],
   plugins: [require("daisyui")],
+  daisyui: {
+    darkTheme: false,
+    themes: [
+      {
+        mytheme: {
+          "primary": "#0577BE",
+          "secondary": "#F3FCFF",
+          "accent": "#F06292",
+          // "neutral": "#F2F2F2",
+          "base-100": "#F8F8F8",
+          "base-200": "#F2F2F2",
+          "base-300": "#D9D9D9",
+        },
+      },
+    ],
+  }
 }
