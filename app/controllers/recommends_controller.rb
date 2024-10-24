1e3fd726de52d94c0f1ce415a1c0998452ce879b
mana <m.komori6517@gmail.com>
2024-10-24 23:08:54 +0900

    add: おすすめアイテム編集・削除のルーティング、コントローラー

--- app/controllers/recommends_controller.rb
+++ app/controllers/recommends_controller.rb
@@ -1,4 +1,6 @@
 class RecommendsController < ApplicationController
+  skip_before_action :authenticate_user!, only: %i[index show]
+
   def index
     @recommends = Recommend.includes(:user).all.group_by(&:place)
   end
@@ -24,6 +26,26 @@ class RecommendsController < ApplicationController
       flash.now[:alert] = t("defaults.flash_message.not_created", item: Recommend.model_name.human)
       render :new, status: :unprocessable_entity
     end
+  end
+
+  def edit
+    @recommend = current_user.recommends.find(params[:id])
+  end
+
+  def update
+    @recommend = current_user.recommends.find(params[:id])
+    if @recommend.update(recommend_params)
+      redirect_to recommend_path(@recommend), notice: t("defaults.flash_message.updated", item: Recommend.model_name.human)
+    else
+      flash.now[:alert] = t("defaults.flash_message.not_updated", item: Recommend.model_name.human)
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    recommend = current_user.recommends.find(params[:id])
+    recommend.destroy!
+    redirect_to recommends_path, notice: t("defaults.flash_message.deleted", item: Recommend.model_name.human), status: :see_other
   end
 
   private
