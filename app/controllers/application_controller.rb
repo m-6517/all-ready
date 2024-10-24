ff07dbfeb4e902b85bbfab5261c50b6da10806e5
mana <m.komori6517@gmail.com>
2024-10-24 23:11:32 +0900

    add: authenticate_user!

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,10 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+
+  before_action :authenticate_user!
+
+  def not_authenticated
+    redirect_to new_user_session_path, alert: t("defaults.flash_message.require_login")
+  end
 end
