708e42f8c6fcb38ffcd78d959c07b734b897a2a0
mana <m.komori6517@gmail.com>
2024-10-24 20:25:46 +0900

    fix: rubocop

--- db/migrate/20241024055250_create_recommends.rb
+++ db/migrate/20241024055250_create_recommends.rb
@@ -2,7 +2,7 @@ class CreateRecommends < ActiveRecord::Migration[7.2]
   def change
     create_table :recommends do |t|
       t.string :place, null: false
-      t.string :item, null:false
+      t.string :item, null: false
       t.text :body
       t.references :user, foreign_key: true
 
