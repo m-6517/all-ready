367b08493af013fe2db972d8094719cff1c8cf8b
mana <m.komori6517@gmail.com>
2024-10-25 12:02:14 +0900

    add: Amazon S3の設定

---
+++ config/initializers/carrierwave.rb
@@ -0,0 +1,22 @@
+require "carrierwave/storage/abstract"
+require "carrierwave/storage/file"
+require "carrierwave/storage/fog"
+
+CarrierWave.configure do |config|
+  if Rails.env.production?
+    config.storage :fog
+    config.fog_provider = "fog/aws"
+    config.fog_directory  = ENV["AWS_BUCKET_NAME"]
+    config.fog_public = false
+    config.fog_credentials = {
+      provider: "AWS",
+      aws_access_key_id:     Rails.application.credentials.dig(:aws, :access_key_id),
+      aws_secret_access_key: Rails.application.credentials.dig(:aws, :secret_access_key),
+      region:                Rails.application.credentials.dig(:aws, :region),
+      path_style: true
+    }
+  else
+    config.storage :file
+    config.enable_processing = false if Rails.env.test?
+  end
+end
