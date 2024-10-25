94f0a2c46b5dfd45b0c04d614b98764b4d073b81
mana <m.komori6517@gmail.com>
2024-10-25 22:08:07 +0900

    add: 持ち物リスト詳細・作成のルーティング、コントローラー

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
     end
   end
 
-  resources :item_lists, only: %i[index]
+  resources :item_lists, only: %i[index show new create]
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
