2089d3acc39a2ce128c07652e3a03859e7524ea9
mana <m.komori6517@gmail.com>
2024-10-28 23:46:51 +0900

    add: original_itemsテーブルにpositionカラムとquantityカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_10_27_010514) do
+ActiveRecord::Schema[7.2].define(version: 2024_10_28_144447) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -38,6 +38,8 @@ ActiveRecord::Schema[7.2].define(version: 2024_10_27_010514) do
     t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "position"
+    t.integer "quantity"
     t.index ["item_list_id"], name: "index_original_items_on_item_list_id"
     t.index ["user_id"], name: "index_original_items_on_user_id"
   end
