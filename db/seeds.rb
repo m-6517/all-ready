4237cc3c2c3550d223ec48b0b887965ba44487c8
mana <m.komori6517@gmail.com>
2024-11-03 12:21:12 +0900

    update: item_statusを追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,20 @@
-default_items = DefaultItem.find_or_create_by([
+default_items = [
   { name: "スマートフォン", position: 1, quantity: 1, selected: false },
   { name: "鍵", position: 2, quantity: 1, selected: false },
   { name: "財布", position: 3, quantity: 1, selected: false }
-])
+].map do |item_data|
+  DefaultItem.find_or_create_by(name: item_data[:name]) do |item|
+    item.position = item_data[:position]
+    item.quantity = item_data[:quantity]
+    item.selected = item_data[:selected]
+  end
+end
 
 ItemList.find_each do |item_list|
   default_items.each do |default_item|
-    ItemListDefaultItem.create(item_list_id: item_list.id, default_item_id: default_item.id)
+    ItemListDefaultItem.find_or_create_by(item_list_id: item_list.id, default_item_id: default_item.id)
+
+    item_status = ItemStatus.find_or_create_by(item_list_id: item_list.id, default_item_id: default_item.id)
+    item_status.update(is_checked: false)
   end
 end
