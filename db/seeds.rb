c308d7226b0868e7b3e2e4c458513732150cbfe3
mana <m.komori6517@gmail.com>
2024-11-08 00:11:20 +0900

    upload: selectedをitem_statusで管理

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,11 @@
 default_items = [
-  { name: "スマートフォン", position: 1, quantity: 1, selected: false },
-  { name: "鍵", position: 2, quantity: 1, selected: false },
-  { name: "財布", position: 3, quantity: 1, selected: false }
+  { name: "スマートフォン", position: 1, quantity: 1 },
+  { name: "鍵", position: 2, quantity: 1 },
+  { name: "財布", position: 3, quantity: 1 }
 ].map do |item_data|
   DefaultItem.find_or_create_by(name: item_data[:name]) do |item|
     item.position = item_data[:position]
     item.quantity = item_data[:quantity]
-    item.selected = item_data[:selected]
   end
 end
 
@@ -14,7 +13,9 @@ ItemList.find_each do |item_list|
   default_items.each do |default_item|
     ItemListDefaultItem.find_or_create_by(item_list_id: item_list.id, default_item_id: default_item.id)
 
-    item_status = ItemStatus.find_or_create_by(item_list_id: item_list.id, default_item_id: default_item.id)
-    item_status.update(is_checked: false)
+    item_status = ItemStatus.find_or_create_by(item_list_id: item_list.id, default_item_id: default_item.id) do |status|
+      status.is_checked = false
+      status.selected = false
+    end
   end
 end
