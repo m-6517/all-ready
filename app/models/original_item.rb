f9d527692e0c4350da282b686aee0c2e52658be1
mana <m.komori6517@gmail.com>
2024-11-08 00:12:10 +0900

    fix: item_list_idカラムの削除にともないバリデーションを削除

--- app/models/original_item.rb
+++ app/models/original_item.rb
@@ -1,12 +1,11 @@
 class OriginalItem < ApplicationRecord
   belongs_to :user
-  has_many :item_list_original_items, dependent: :destroy
+  has_many :item_list_original_items
   has_many :item_lists, through: :item_list_original_items
   has_many :item_statuses, dependent: :destroy
 
   validates :user, presence: true
   validates :name, presence: true
-  validates :item_list_id, presence: true
   validates :quantity, numericality: { greater_than_or_equal_to: 0 }
   validates :position, numericality: { only_integer: true, allow_nil: true }
 end
