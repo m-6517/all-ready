1c0503cd89025fe30e3d78075703b97978cac691
mana <m.komori6517@gmail.com>
2024-11-18 18:01:41 +0900

    fix: rubocop

--- app/controllers/bag_contents_controller.rb
+++ app/controllers/bag_contents_controller.rb
@@ -25,21 +25,21 @@ class BagContentsController < ApplicationController
 
   def create
     @bag_content = @item_list.bag_contents.find_by(user: current_user)
-  
+
     if @item_list.original_items.empty? && @item_list.default_items.empty?
       redirect_to item_list_path(@item_list), alert: "空の持ち物リストは共有できません"
       return
     end
-  
+
     if @bag_content.nil?
       @bag_content = BagContent.new(bag_content_params)
     end
-  
+
     if current_user.bag_contents.exists?(item_list_id: @item_list.id)
       redirect_to item_list_path(@item_list), alert: "この持ち物リストは既に共有されています"
     else
       @bag_content = current_user.bag_contents.new(bag_content_params.merge(item_list: @item_list))
-  
+
       if @bag_content.save_with_tags(tag_name: params.dig(:bag_content, :tag_name).split(",").uniq)
         redirect_to item_list_bag_contents_path(@item_list), notice: "持ち物リストを共有しました"
       else
@@ -55,7 +55,7 @@ class BagContentsController < ApplicationController
   def update
     @bag_content = current_user.bag_contents.find(params[:id])
     @bag_content.body = bag_content_params[:body]
-  
+
     if @bag_content.save_with_tags(tag_name: params.dig(:bag_content, :tag_name).split(",").uniq)
       redirect_to bag_contents_path(@bag_content), notice: "投稿を更新しました"
     else
