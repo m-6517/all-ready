b137f149d9308121ae602fc0f274224a66fb54e6
mana <m.komori6517@gmail.com>
2024-11-27 22:34:38 +0900

    fix: コントローラー、deviseの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,7 +11,7 @@ class User < ApplicationRecord
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
-         :omniauthable, omniauth_providers: [:google_oauth2]
+         :omniauthable, omniauth_providers: [ :google_oauth2 ]
 
   mount_uploader :avatar, AvatarUploader
 
