4dbe510fb52f9c87912fc3429279a561efea6f41
mana <m.komori6517@gmail.com>
2024-12-07 20:39:14 +0900

    add: ブックマークのコントローラー

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,19 @@
+class BookmarksController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+    @bookmarked_recommends = current_user.bookmarks.includes(:bookmarkable).map(&:bookmarkable)
+  end
+
+  def create
+    bookmarkable_uuid = params[:bookmarkable]
+    @recommend = Recommend.find_by(uuid: bookmarkable_uuid)  
+    current_user.bookmark(@recommend)
+  end
+
+  def destroy
+    bookmark = current_user.bookmarks.find(params[:id])
+    @recommend = bookmark.bookmarkable
+    current_user.unbookmark(@recommend)
+  end
+end
