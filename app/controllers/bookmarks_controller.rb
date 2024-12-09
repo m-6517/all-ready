e23c01521fb230fa503ed4afe2aabbefd8bcc9ad
mana <m.komori6517@gmail.com>
2024-12-09 21:09:49 +0900

    add: ポリモーフィック関連付けによりマストアイテムとみんなの持ち物の両方をブックマークできるよう設定

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -3,17 +3,26 @@ class BookmarksController < ApplicationController
 
   def index
     @bookmarked_recommends = current_user.bookmarks.includes(:bookmarkable).map(&:bookmarkable)
+    @bookmarked_bag_contents = current_user.bookmarks.includes(:bookmarkable).map(&:bookmarkable)
   end
 
   def create
     bookmarkable_uuid = params[:bookmarkable]
     @recommend = Recommend.find_by(uuid: bookmarkable_uuid)
-    current_user.bookmark(@recommend)
+    @bag_content = BagContent.find_by(uuid: bookmarkable_uuid)
+    @bookmarkable = @recommend || @bag_content
+    current_user.bookmark(@bookmarkable)
   end
 
   def destroy
     bookmark = current_user.bookmarks.find(params[:id])
-    @recommend = bookmark.bookmarkable
-    current_user.unbookmark(@recommend)
+    if bookmark.bookmarkable_type == 'Recommend'
+      @recommend = bookmark.bookmarkable
+      @bag_content = nil
+    else bookmark.bookmarkable_type == 'BagContent'
+      @bag_content = bookmark.bookmarkable
+      @recommend = nil
+    end
+    current_user.unbookmark(@recommend || @bag_content)
   end
 end
