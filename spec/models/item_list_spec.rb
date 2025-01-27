3e7a56c57e28c54f8dad40e88a22c647f6f275e3
mana <m.komori6517@gmail.com>
2025-01-27 18:21:13 +0900

    fix: モデルスペックとバリデーションを修正

--- spec/models/item_list_spec.rb
+++ spec/models/item_list_spec.rb
@@ -6,11 +6,16 @@ RSpec.describe ItemList, type: :model do
     expect(item_list).to be_valid
   end
 
-  it 'リスト名、ユーザー情報は必須項目であること' do
-    item_list = build(:item_list, name: nil, user_uuid: nil)
+  it 'リスト名は必須項目であること' do
+    item_list = build(:item_list, name: nil)
     item_list.valid?
     expect(item_list.errors[:name]).to include('を入力してください')
-    expect(item_list.errors[:user_uuid]).to include('を入力してください')
+  end
+
+  it 'ユーザー情報がなければリストを作成できないこと' do
+    item_list = build(:item_list, user_uuid: nil)
+    item_list.valid?
+    expect(item_list.errors[:user_uuid]).to include('は必須です')
   end
 
   it '準備状況は0以上100以下の数値であること' do
