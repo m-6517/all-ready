6c11197efec030f6cabb4504fb0ddc232ab6b817
mana <m.komori6517@gmail.com>
2025-02-05 00:22:12 +0900

    fix: 全てのdefault_itemが投稿に表示されるのを修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -29,22 +29,29 @@ default_items = [
   { name: "ヘッドホン" },
   { name: "めがね" },
   { name: "コンタクトレンズ" }
-
 ].map do |item_data|
   DefaultItem.find_or_create_by(name: item_data[:name])
 end
 
 ItemList.find_each do |item_list|
   default_items.each_with_index do |default_item, index|
-    ItemListDefaultItem.find_or_create_by(item_list_id: item_list.id, default_item_id: default_item.id)
+    ItemListDefaultItem.find_or_create_by(
+      item_list_id: item_list.id,
+      default_item_id: default_item.id
+    )
 
-    item_status = ItemStatus.find_or_create_by(item_list_id: item_list.id, default_item_id: default_item.id) do |status|
-      status.is_checked = false
-      status.selected = false
-      status.quantity = 1
-    end
+    item_status = ItemStatus.find_or_initialize_by(
+      item_list_id: item_list.id,
+      default_item_id: default_item.id
+    )
 
-    if item_status.position.nil?
+    if item_status.new_record?
+      item_status.is_checked = false
+      item_status.selected = false
+      item_status.quantity = 1
+      item_status.position = index + 1
+      item_status.save!
+    elsif item_status.position.nil?
       item_status.update(position: index + 1)
     end
   end
