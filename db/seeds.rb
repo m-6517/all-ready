aa35dd02691ed398cde8f74b881c97c0ce716585
mana <m.komori6517@gmail.com>
2025-02-04 10:27:53 +0900

    upload: 既存のアイテムのシードデータ

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,35 @@
 default_items = [
   { name: "スマートフォン" },
   { name: "鍵" },
-  { name: "財布" }
+  { name: "財布" },
+  { name: "交通系ICカード" },
+  { name: "乗車券" },
+  { name: "航空券" },
+  { name: "パスポート" },
+  { name: "ハンカチ" },
+  { name: "ティッシュ" },
+  { name: "ウェットティッシュ" },
+  { name: "常備薬" },
+  { name: "目薬" },
+  { name: "マスク" },
+  { name: "絆創膏" },
+  { name: "折りたたみ傘" },
+  { name: "日傘" },
+  { name: "エコバッグ" },
+  { name: "マイボトル" },
+  { name: "時計" },
+  { name: "PC" },
+  { name: "タブレット" },
+  { name: "モバイルバッテリー" },
+  { name: "ポケット型Wi-Fi" },
+  { name: "充電器" },
+  { name: "マウス" },
+  { name: "キーボード" },
+  { name: "イヤホン" },
+  { name: "ヘッドホン" },
+  { name: "めがね" },
+  { name: "コンタクトレンズ" },
+
 ].map do |item_data|
   DefaultItem.find_or_create_by(name: item_data[:name])
 end
