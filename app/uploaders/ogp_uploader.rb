1628793b81e38dfb3ab7427b0d96d2db77369b22
mana <m.komori6517@gmail.com>
2025-02-05 11:50:48 +0900

    fix: 画像の保存場所を変更

--- app/uploaders/ogp_uploader.rb
+++ app/uploaders/ogp_uploader.rb
@@ -9,9 +9,11 @@ class OgpUploader < CarrierWave::Uploader::Base
 
   def store_dir
     if model.is_a?(Recommend)
-      "uploads/ogp_images/recommend/#{model.id}"
-    else model.is_a?(BagContent)
-      "uploads/ogp_images/bag_content/#{model.id}"
+      "uploads/ogp_images/recommends/#{model.id}"
+    elsif model.is_a?(BagContent)
+      "uploads/ogp_images/bag_contents/#{model.id}"
+    else
+      "uploads/ogp_images/#{model.class.to_s.underscore}/#{model.id}"
     end
   end
 
