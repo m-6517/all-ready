d9050c37ad081558caba8431b27d8747b1cc1575
mana <m.komori6517@gmail.com>
2025-02-07 02:30:14 +0900

    upload: マストアイテムを投稿後、詳細画面に遷移

--- app/controllers/recommends_controller.rb
+++ app/controllers/recommends_controller.rb
@@ -23,7 +23,7 @@ class RecommendsController < ApplicationController
   def create
     @recommend = current_user.recommends.build(recommend_params)
     if @recommend.save
-      redirect_to recommends_path, notice: t("defaults.flash_message.created", item: Recommend.model_name.human)
+      redirect_to recommend_path(@recommend), notice: t("defaults.flash_message.created", item: Recommend.model_name.human)
     else
       flash.now[:alert] = t("defaults.flash_message.not_created", item: Recommend.model_name.human)
       render :new, status: :unprocessable_entity
