0f19ba28e2aa1cf95b92d98d2379fba5479d1fbf
mana <m.komori6517@gmail.com>
2025-02-11 10:53:40 +0900

    fix: rubocop

--- app/controllers/bag_contents_controller.rb
+++ app/controllers/bag_contents_controller.rb
@@ -29,7 +29,7 @@ class BagContentsController < ApplicationController
     @bag_content = current_user.bag_contents.new(item_list_id: @item_list.id)
     tag_names = params[:tag_name]
     body = params[:body]
-  
+
     if @item_list.original_items.empty? && @item_list.default_items.empty?
       flash[:alert] = t("defaults.flash_message.empty_list", item: BagContent.model_name.human)
       redirect_to item_list_path(@item_list)
@@ -46,7 +46,7 @@ class BagContentsController < ApplicationController
         render :new, alert: t("defaults.flash_message.not_shared", item: BagContent.model_name.human)
       end
     end
-  end  
+  end
 
   def edit
     @bag_content = current_user.bag_contents.find_by(uuid: params[:id])
