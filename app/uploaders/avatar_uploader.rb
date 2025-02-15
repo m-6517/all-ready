cf4db905cd61d497fd45d4ed56089d52205a43c5
mana <m.komori6517@gmail.com>
2025-02-15 22:39:11 +0900

    fix: アップロード可能なファイル形式にwebpを追加

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -42,8 +42,8 @@ class AvatarUploader < CarrierWave::Uploader::Base
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
-  def extension_whitelist
-    %w[jpg jpeg gif png]
+  def extension_allowlist
+    %w[jpg jpeg gif png webp]
   end
 
   # Override the filename of the uploaded files:
